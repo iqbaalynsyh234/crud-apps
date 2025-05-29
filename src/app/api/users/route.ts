94dd21dd0522798bef6c023c5420a09c80576a51
iqbalalynsyh234 <iqbalalyansyah3@gmail.com>
2025-05-29 20:32:49 +0700

    fixing bug error user crud ✔🎁

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@/src/generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
